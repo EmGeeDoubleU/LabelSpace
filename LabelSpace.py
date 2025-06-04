20546af4e657d518e598f61183549d90730c2427
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2025-06-04 18:55:20 -0400

    Label now shows up in bar.
    Im going to need to optomize it since it uses a lot of compute
    
    Co-Authored-By: Michael Grant Warshowksy <20256346+emgeedoubleu@users.noreply.github.com>

--- LabelSpace.py
+++ LabelSpace.py
@@ -1,19 +1,23 @@
 import subprocess
 import time
 import json
+import rumps
+
+
 SPACE_LABELS = [
-    "SCHOOL",
-    "JOB SEARCH",
-    "LASSO",
-    "PERSONAL"
+    "🟦 SCHOOL",
+    "🟩 PERSONAL",
+    "🟥 JOB SEARCH",
+    "🟨 LASSO"
 ]
+
 def start_yabai() -> None:
     command = ["/opt/homebrew/bin/yabai", "--verbose"]
     subprocess.Popen(command) # this will open the socket for yabai in the background so that the rest of the script doesnt get held up
 
 def run_yabai() -> str:
     command = ["/opt/homebrew/bin/yabai", "-m", "query", "--spaces"]
-    print(f"running command {' '.join(command)}")
+    #print(f"running command {' '.join(command)}")
     #print("stdout:",result.stdout)
     #print("stderr:", result.stderr)
     while True:
@@ -29,19 +33,26 @@ def get_screen_index(json_output: str) -> int:
     screens = json.loads(json_output)
     for screen in screens:
         if screen.get("is-visible") :
-            print(screen.get("index"))
+            #print(screen.get("index"))
             return screen.get("index")
     return -1
 
 
 def label_screen(index: int) -> str:
-    print(SPACE_LABELS[index-1])
+    #print(SPACE_LABELS[index-1])
     return SPACE_LABELS[index-1]
 
 
-if __name__ == '__main__':
-    
-     # wait a second for the socket to spin up
-    json_output: str = run_yabai() # returns screen index as json
-    current_index: int = get_screen_index(json_output) # finds the current screen
-    label_screen(current_index)
+
+app = rumps.App("Loading...")
+
+def update_label(_):
+    json_output = run_yabai()
+    current_index = get_screen_index(json_output)
+    label = label_screen(current_index)
+    app.title = label
+
+timer = rumps.Timer(update_label, .05)
+timer.start()
+
+app.run()
